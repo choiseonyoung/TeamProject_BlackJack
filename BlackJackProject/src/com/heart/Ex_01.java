62d91b0fe48113b6375ddc203b8360b94fd62bb5
choiseonyoung <bviibb@naver.com>
2021-04-22 03:52:15 +0900

    gamerMoney() 수정

--- BlackJackProject/src/com/heart/Ex_01.java
+++ BlackJackProject/src/com/heart/Ex_01.java
@@ -1,11 +1,11 @@
 package com.heart;
 
-import com.heart.impl.BlackJackRuleImplV7;
+import com.heart.impl.BlackJackRuleImplV8;
 
 public class Ex_01 {
 
 	public static void main(String[] args) {
-		BlackJackRuleImplV7 bj = new BlackJackRuleImplV7();
+		BlackJackRuleImplV8 bj = new BlackJackRuleImplV8();
 		bj.gameMainScreen();
 	}
 	
--- BlackJackProject/src/com/heart/impl/BlackJackYubin.java
+++ BlackJackProject/src/com/heart/impl/BlackJackYubin.java
@@ -8,16 +8,18 @@ import com.heart.model.DeckVO;
 
 public class BlackJackYubin{
 
-	List<DeckVO> deckList;
-	String[] suit;
-	String[] strNum;
+	public List<DeckVO> deckList;
+	protected String[] suit;
+	protected String[] strNum;
+	
+	
 	public BlackJackYubin() {
 		deckList= new ArrayList<DeckVO>();
 		suit =  new String[] {"Heart","Diamond","Clover","Space"};
 		strNum = new String[] {"A","2","3","4","5","6","7","8","9","10","K","Q","J"};
 	}
 	
-	public void createDeck(){ 
+	public void createDeck(List<DeckVO> deckList){ 
 		//TODO 카드형식을 만들고 각 카드에 따른 값을 만듬
 		for(int i = 0; i < suit.length ; i++) {
 			for(int j = 0 ; j < strNum.length ; j++) {
